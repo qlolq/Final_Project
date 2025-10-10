07b9080ba9cf5c3ff5bb31c2eee498426283f43e
Peter_Chow <chowyikho123@gmail.com>
2025-10-10 22:34:15 +0800

    10/10/2025,updated walking

---
+++ Assets/Script/Swordsman_property.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordsman_property : character_property
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "Swordsman";
+        hp = 456;
+        atk = 55;
+        def = 20;
+        satk = 0;
+        sdef = 15;
+        speed = 3.0f;
+        //dex;  
+        atkRange = 0.8f;
+        //effectRange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
