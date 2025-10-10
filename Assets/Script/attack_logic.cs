ab130609c962f712885ecb88b6050a52af33b400
heytsang925 <tsanglokhey@gmail.com>
2025-10-10 22:02:03 +0800

    UI

---
+++ Assets/Script/attack_logic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack_logic : action
+{
+    private move_logic moveLogic;
+
+    void Start()
+    {
+        moveLogic = GetComponent<move_logic>();
+
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsAttacking", false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void AutoPilot()
+    {
+        animator.SetBool("IsAttacking", true);
+
+        if (this.transform.position.x < target.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+    }
+
+    void StopPilot()
+    {
+        animator.SetBool("IsAttacking", false);
+    }
+
+    void Update()
+    {
+        if (moveLogic.CalculateMagnitude() <= 0.8f)
+        {
+            AutoPilot();
+        }
+
+        else if (moveLogic.CalculateMagnitude() > 0.8f)
+        {
+            StopPilot();
+        }
+    }
+}
---
+++ Assets/Script/move_logic.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_logic : action
+{
+    private float speed = 3.0f;
+
+    // Bool to turn AutoPilot On/Off
+    bool autoPilot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsWalking", false);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    Vector3 CalculateDistance() 
+    {
+        Vector3 fD = target.transform.position - this.transform.position; 
+        return fD;
+    }
+
+    internal float CalculateMagnitude() { 
+        return CalculateDistance().magnitude;
+    }
+
+    void AutoPilot()
+    {
+        Vector3 movement = CalculateDistance();
+        movement.Normalize();
+        this.transform.Translate(movement * speed * Time.deltaTime);
+        animator.SetBool("IsWalking", true);
+
+        if (this.transform.position.x < target.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void StopPilot() {
+        animator.SetBool("IsWalking", false);
+    }
+
+    void Update()
+    {
+        // Check if the T key has been pressed
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            autoPilot = !autoPilot;
+        }
+
+        if (autoPilot == true)
+        {
+
+            if (CalculateMagnitude() > 0.8f)
+            {
+                AutoPilot();
+            }
+
+            else if (CalculateMagnitude() <= 0.8f)
+            {
+                StopPilot();
+            }
+        }
+
+        else if (autoPilot == false) {
+            StopPilot();
+        }
+    }
+}
