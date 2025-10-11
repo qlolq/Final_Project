7f69d75e269617b02a37113565a9afadcfac1c36
heytsang925 <tsanglokhey@gmail.com>
2025-10-11 16:21:24 +0800

    UI

--- Assets/Script/character_property.cs
+++ Assets/Script/character_property.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class character_property : MonoBehaviour
 {
-    char name; 
-    int hp;
-    int atk;
-    int def;
-    int satk; //special attack,通常指定為魔法攻擊    
-    int sdef; //special defence,通常指定為魔法防禦
-    int speed;
-    int dex;  //dex是指攻擊速度（攻擊的頻率）
-    int atkRange; //攻擊範圍（手長短）只for普通攻擊
-    int effectRange; //傷害判定範圍（AOE？單體攻擊？）只for普通攻擊
+    protected string name;
+    protected int hp;
+    protected int atk;
+    protected int def;
+    protected int satk; //special attack,通常指定為魔法攻擊    
+    protected int sdef; //special defence,通常指定為魔法防禦
+    protected float speed;
+    protected float dex;  //dex是指攻擊速度（攻擊的頻率）
+    protected float atkRange; //攻擊範圍（手長短）只for普通攻擊
+    protected float effectRange; //傷害判定範圍（AOE？單體攻擊？）只for普通攻擊
     
     // Start is called before the first frame update
     void Start()
